734e29df6b74c8acfc44adb04951d2b8063489e1
Joe Caulfield <joe@caulfieldengineering.com>
2025-06-06 20:14:01 -0400

    Initial Configuration

--- config.cmake
+++ config.cmake
@@ -1,11 +1,11 @@
 # config.cmake
 
 # Define global variables
-set(wPLUGIN_NAME                "NewPluginTemplate")
-set(wCOMPANY_NAME               "Wonderland Audio")
-set(wCOMPANY_ID                 "WonderlandAudio")
+set(wPLUGIN_NAME                "WizardOne")
+set(wCOMPANY_NAME               "Mr. Wizard FX")
+set(wCOMPANY_ID                 "MrWizardFX")
 set(wPROJECT_VERSION            "0.1.0")
-set(wPLUGIN_MANUFACTURER_CODE   "WLAD")
+set(wPLUGIN_MANUFACTURER_CODE   "MRWZ")
 set(wFORMATS                    VST3 Standalone)
 
 set(wIS_SYNTH                   FALSE)
